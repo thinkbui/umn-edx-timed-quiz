c560e2bf998050d6240090a6416c7a7e1f1d2e6d
Ian Bui <thinkbui@gmail.com>
2023-04-04 00:47:47 -0500

    Added click response to answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("Script present");
+// console.log("Script present");
+
+var answerElems = document.getElementsByClassName("answer");
+for (i=0;i<answerElems.length;i++) {
+  // console.log(answerElems[i].id);
+  answerElems[i].addEventListener("click", answerHandler);
+}
+
+function answerHandler(event) {
+  var answer_val = event.currentTarget.getAttribute("data-val");
+  document.getElementsByClassName("message")[0].textContent = answer_val;
+}
