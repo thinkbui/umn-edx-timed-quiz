134e13368f3f1c463adf2212f5aca6f7447eb2a3
Ian Bui <thinkbui@gmail.com>
2023-04-05 23:32:33 -0400

    Added structure of high score page and separated high score JS functions to separate file

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var currentQuestionIndex;
 var messageElem = document.getElementsByClassName("message")[0];
 var beginQuizElem = document.getElementsByClassName("quiz_begin")[0];
 var beginQuizButton = beginQuizElem.getElementsByTagName("button")[0];
-var questionBoxElem = document.getElementsByTagName("article")[0];
+var questionBoxElem = document.getElementsByClassName("question_content")[0];
 
 var answerElems = document.getElementsByClassName("answer");
 for (i=0;i<answerElems.length;i++) {
@@ -149,27 +149,4 @@ function setQuickFlashMessage(message1,message2) {
 // Displays the flash message
 function setFlashMessage(message) {
   messageElem.textContent = message;
-}
-
-// Adds the user score to the high score list, sorts descending by score, then truncates the list if too long
-function scoreHandler(player_initials){
-  high_scores = getHighScores();
-  high_scores.push([player_initials, score]);
-  high_scores.sort(function(a,b) {return b[1] - a[1]});
-  setHighScores(high_scores);
-}
-
-// Fetches the high scores from local storage
-function getHighScores() {
-  var high_scores_json = localStorage.getItem("umn-edx-timed-quiz-high-scores");
-  if (!high_scores_json) {
-    return [];
-  }
-  return JSON.parse(high_scores_json)["scores"];
-}
-
-// Stores the high scores into local storage
-function setHighScores(score_data) {
-  var high_scores_json = JSON.stringify({scores: score_data});
-  localStorage.setItem("umn-edx-timed-quiz-high-scores", high_scores_json);
 }
